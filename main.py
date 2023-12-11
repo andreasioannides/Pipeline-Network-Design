b65d2ab43f1c8695fcce0f63c5b39e28def6f29b
AnIo <92021232+andreasioannides@users.noreply.github.com>
2023-12-11 21:53:37 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -26,45 +26,43 @@ def correct_pressures(network: object):
 
     num_nodes = len(network.nodesList)
     bc_nodes = params["boundary_conditions_nodes"]  # list with the indexes of nodes with boundary conditions
-    nodesList = network.nodesList
-    J = np.zeros(shape=[num_nodes, num_nodes], dtype=float)  
-    F = np.zeros(shape=[num_nodes, 1], dtype=float)  
-    der = 0  # initialize the derivative of Q with respect to h
-    p_old = nodesList[:, 2]  # pressures
-    edge = 0
+    dP_old = 0
 
     while True:
+        J = np.zeros(shape=[num_nodes, num_nodes], dtype=float)  
+        F = np.zeros(shape=[num_nodes, 1], dtype=float)  
+        der = 0  # initialize the derivative of Q with respect to h
+        nodesList = network.nodesList
+
         for i, node in enumerate(nodesList):
             if (i+1 in bc_nodes):  # check if the node has boundary conditions
                 J[i, i] = 1
                 continue
 
-            for edge_idx in network.connectedEdges:  
-                edge = network.edgesList[edge_idx]
-                print(edge[0])         
+            for edge_idx in network.connectedEdges[i]:  
+                edge = network.edgesList[edge_idx]         
                 F[i] += network.sign_Q(edge_idx, i)
-                der = 0.5 * 1/sqrt(edge[6]) * 1/sqrt(abs(edge[5]))
+                der = 0.5 * 1/sqrt(edge[6]* 9.81 * params["density"]) * 1/sqrt(abs(edge[5]))  # transform pressure from mΣΥ=Pa/(ρ*g) to Pa
                 J[i, i] -= der
 
                 if (i == int(edge[0])):
                     J[i, int(edge[1])] = der
-                    # J[i, network.nodesList.index(edge.nodeB)] = der
                 else:
-                    # J[i, network.nodesList.index(edge.nodeA)] = der
                     J[i, int(edge[0])] = der
 
             F[i] += node[3]  # add consumption
 
-        dP = np.linalg.solve(J, F)
-        p_new = p_old + dP
+        dP = np.linalg.solve(J, -F)  # J*dP + F = 0 -> J*dP = -F
+        dP = np.squeeze(dP)
+        nodesList[:, 2] += dP
+        network.edgeAttributes(init=False)
 
-        if (((p_new-p_old)/p_new).all() <= 1.0e-7):  # check if the loop has converged
+        if (np.all(np.abs(dP - dP_old) <= 1.0e-10)):  # check if the loop has converged
             break
+        
+        dP_old = dP
 
-        p_old = p_new
-
-        print(J)
-        break
+    print(nodesList[:, 2])
         
 def main():
     path = "network_data.xlsx"
@@ -75,8 +73,6 @@ def main():
     # net.plotNetwork()
     
     correct_pressures(net)
-    # J, F = correct_pressures(net)
-    # print(J)
 
 if __name__ == '__main__':
     main()
