f6398828965fe8c73eeb3d5d3a06ebefad3fbfd1
AnIo <92021232+andreasioannides@users.noreply.github.com>
2023-12-13 22:55:29 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,78 +1,94 @@
-import openpyxl as xl
 import numpy as np
-from network import createNetwork
-import json
-from math import sqrt
+from network import createNetwork, load_data
+import openpyxl as xl
+from math import sqrt, pi
 
-# Initialize Global Parameters
-with open('params.json', 'r') as file:
-    params = json.load(file)
-
-def load_data(path: str, sheet_name: str) -> np.ndarray:
-    '''Load the data from excel.'''
-
-    wb = xl.load_workbook(path)
-    data = []
-
-    for row in wb[sheet_name].iter_rows(min_row=2, values_only=True):
-        data.append(list(row))
-
-    data = np.array(data)
-
-    return data
-
-def correct_pressures(network: object):
-    '''Correct the pressures of the nodes given the initial pressures.'''
-
-    num_nodes = len(network.nodesList)
-    bc_nodes = params["boundary_conditions_nodes"]  # list with the indexes of nodes with boundary conditions
+def iterations(network: object, params: object, iter: int):
     dP_old = 0
 
+    j = 0
     while True:
-        J = np.zeros(shape=[num_nodes, num_nodes], dtype=float)  
-        F = np.zeros(shape=[num_nodes, 1], dtype=float)  
-        der = 0  # initialize the derivative of Q with respect to h
-        nodesList = network.nodesList
-
-        for i, node in enumerate(nodesList):
-            if (i+1 in bc_nodes):  # check if the node has boundary conditions
-                J[i, i] = 1
-                continue
-
-            for edge_idx in network.connectedEdges[i]:  
-                edge = network.edgesList[edge_idx]         
-                F[i] += network.sign_Q(edge_idx, i)
-                der = 0.5 * 1/sqrt(edge[6]* 9.81 * params["density"]) * 1/sqrt(abs(edge[5]))  # transform pressure from mΣΥ=Pa/(ρ*g) to Pa
-                J[i, i] -= der
-
-                if (i == int(edge[0])):
-                    J[i, int(edge[1])] = der
-                else:
-                    J[i, int(edge[0])] = der
-
-            F[i] += node[3]  # add consumption
-
-        dP = np.linalg.solve(J, -F)  # J*dP + F = 0 -> J*dP = -F
-        dP = np.squeeze(dP)
-        nodesList[:, 2] += dP
-        network.edgeAttributes(init=False)
+        # print(f"Iter: {j}")
+        dP = correct_pressures(network, params)    
 
         if (np.all(np.abs(dP - dP_old) <= 1.0e-10)):  # check if the loop has converged
             break
         
+        j += 1
+
+        if (j == iter):
+            # print(network.edgesList[:, 5])
+            iter_1(network)
+            break
+        #     elif (iter == 2):
+                        
         dP_old = dP
 
-    print(nodesList[:, 2])
-        
+    print(network.nodesList[:, 2])  
+
+def correct_pressures(network: object, params: object) -> np.ndarray:
+    '''Correct the pressures of the nodes.'''
+
+    num_nodes = len(network.nodesList)  
+    bc_nodes = [cell.value for cell in params['E'][1:]]  # list with the indexes of nodes with boundary conditions
+
+    J = np.zeros(shape=[num_nodes, num_nodes], dtype=float)  
+    F = np.zeros(shape=[num_nodes, 1], dtype=float)  
+    der = 0  # initialize the derivative of Q with respect to h
+    
+    for i, node in enumerate(network.nodesList):
+        if (i+1 in bc_nodes):  # check if the node has boundary conditions
+            J[i, i] = 1
+            continue
+
+        for edge_idx in network.connectedEdges[i]:  
+            edge = network.edgesList[edge_idx]         
+            F[i] += network.sign_Q(edge_idx, i)
+            der = 0.5 * 1/sqrt(edge[6]* 9.81 * params["D2"].value) * 1/sqrt(abs(edge[5]))  # transform pressure from mΣΥ=Pa/(ρ*g) to Pa
+            J[i, i] -= der
+
+            if (i == int(edge[0])):
+                J[i, int(edge[1])] = der
+            else:
+                J[i, int(edge[0])] = der
+
+        F[i] += node[3]  # add consumption
+
+    dP = np.linalg.solve(J, -F)  # J*dP + F = 0 -> J*dP = -F
+    dP = np.squeeze(dP)
+    network.nodesList[:, 2] += dP
+    network.edgeAttributes(init=False)
+
+    return dP
+
+def iter_1(network):
+    '''Iteration 1: check the direction of each edge's supply.'''
+
+    edges = network.edgesList
+
+    for edge in edges:
+        if (edge[5] > 0):  # dP>0 -> Q: nodeA -> nodeB
+            print(f"\nEdge {int(edge[0])}-{int(edge[1])}: +{edge[7]}")
+        else:  # dP<0 -> Q: nodeB -> nodeA
+            print(f"Edge {int(edge[0])}-{int(edge[1])}: -{edge[7]}")
+
 def main():
     path = "network_data.xlsx"
-    nodes = load_data(path, "Nodes")
+    nodes = load_data(path, "Nodes")  # access the "Nodes" sheet from the excel file
     edges = load_data(path, "Edges")
+    params = xl.load_workbook(path)["Params"]  
     
-    net = createNetwork(nodes, edges)
+    net = createNetwork(nodes, edges, params)
     # net.plotNetwork()
     
-    correct_pressures(net)
+    iterations(net, params, 1)  # check the direction of each edge's supply
+    print(f"\nOpen file: '{path}', sheet'Edges' sheet and update the values of ζ.") 
+    next = input(f"Type [yes] and click enter if you updated the values of ζ: ")
+
+    if (next == "yes"):
+        net.update_zeta(path)  # define the values of ζ for each node
+        # iterations(net, params, 2)  # 
+        # iterations(net, params, None)
 
 if __name__ == '__main__':
     main()
--- network.py
+++ network.py
@@ -1,11 +1,20 @@
 import numpy as np
-from math import sqrt, pi, log10
-import json
+import openpyxl as xl
+from math import pi, sqrt
 import matplotlib.pyplot as plt
 
-# Initialize Global Parameters
-with open('params.json', 'r') as file:
-    params = json.load(file)
+def load_data(path: str, sheet_name: str) -> np.ndarray:
+    '''Load the data from excel.'''
+
+    wb = xl.load_workbook(path)
+    data = []
+
+    for row in wb[sheet_name].iter_rows(min_row=2, values_only=True):
+        data.append(list(row))
+
+    data = np.array(data)
+
+    return data
 
 class createNetwork:
     '''
@@ -30,7 +39,7 @@ class createNetwork:
         Plot the pipeline network.
     '''
 
-    def __init__(self, nodes: np.ndarray, edges: np.ndarray):
+    def __init__(self, nodes: np.ndarray, edges: np.ndarray, params: object):
         '''Default network constructor.'''
 
         self.nodesList = nodes 
@@ -44,8 +53,8 @@ class createNetwork:
         3: Q (consumption in the node)
         '''
 
-        z = np.zeros(shape=(edges.shape[0], 5))
-        self.edgesList = np.concatenate((edges, z), axis=1) 
+        a = np.zeros(shape=(edges.shape[0], 6))
+        self.edgesList = np.concatenate((edges[:, :3], a), axis=1)  # initially set ζ=0
         '''
         Array with all edges of the network. 
 
@@ -58,8 +67,21 @@ class createNetwork:
         5: dP (pressure difference)
         6: k (coefficient of linear losses)
         7: Q (supply)
+        8: ζ (localized losses in the first node of the pipeline)
         '''
 
+        self.params = params
+        '''
+        Get the parameters of the pipeline network.
+
+        Default excel file:
+        column A: D (diameter)
+        column B: epsilon (roughness)
+        column C: viscosity
+        column D: density
+        column E: list with the indexes of nodes with boundary conditions
+        '''
+        
         self.edgeAttributes(init=True)  
         self.connectedEdges = self.get_connected_edges()
         '''Array with the connected edges for each node.'''
@@ -68,8 +90,8 @@ class createNetwork:
         '''Calculate the attributes of the network's edges.'''
 
         if (init):
-            self.edgesList[:, 4] = params["epsilon"]
-            self.edgesList[:, 3] = params["D"]
+            self.edgesList[:, 4] = self.params["B2"].value
+            self.edgesList[:, 3] = sqrt((4*self.nodesList[0, 3]) / (3.5*pi))  # calculate the initial diameter of the pipelines using d=sqrt(4Q/(U*π)) whereU=3.5 m/s and Q is the supply in the first node
             self.edgesList[:, :2] -= 1  # reduce the index of the nodes by 1 for better usability with array indexing
 
         nodeA_idx = np.int32(self.edgesList[:, 0])  # data type casting beacuse the type of nodesList array is float
@@ -100,7 +122,8 @@ class createNetwork:
         Re = 1.0e10  # Reynolds: initial value set to infinite
         edges = self.edgesList
         lambda_ = np.power(1 / (1.14 - 2*np.log10(edges[:, 4]/edges[:, 3] + 21.25/Re**0.9)), 2)  # Jain equation
-        k = lambda_ * (edges[:, 2]/edges[:, 3] * 8 / (pi**2 * 9.81 * np.power(edges[:, 3], 4)))
+        c = 8 / (pi**2 * 9.81 * np.power(edges[:, 3], 4)) 
+        k = lambda_ * (edges[:, 2]/edges[:, 3] * c) + edges[:, 8] * c  # λ(L/D)*8/(π^2*g*D^4) + ζ*8/(π^2*g*D^4)
 
         return k
     
@@ -113,10 +136,11 @@ class createNetwork:
 
     #     while True:
     #         lambda_ = np.power(1 / (1.14 - 2*np.log10(edges[:, 4]/edges[:, 3] + 21.25/Re**0.9)), 2)  # Jain equation
-    #         k = lambda_ * (edges[:, 2]/edges[:, 3] * 8 / (pi**2 * 9.81 * np.power(edges[:, 3], 4)))
+    #         c = 8 / (pi**2 * 9.81 * np.power(edges[:, 3], 4)) 
+    #         k = lambda_ * (edges[:, 2]/edges[:, 3] * c) + edges[:, 8] * c
     #         Q = self.supply()
     #         U = 4*Q / (pi * np.power(edges[:, 3], 2))  # velosity
-    #         Re = U * edges[:, 3] / params["viscosity"]
+    #         Re = U * edges[:, 3] / self.params["C2"].value
 
     #         if (np.all(np.abs((k - k_old)/k) <= 1.0e-7)):  # check if the loop has converged
     #             break
@@ -130,7 +154,7 @@ class createNetwork:
 
         edges = self.edgesList
 
-        return np.sqrt(np.abs(edges[:, 5]) / (edges[:, 6] * 9.81 * params["density"]))  # To transform pressure from mΣΥ=Pa/(ρ*g) to Pa multiply denominator with ρ*g . Q=(|dP|/(K_AB*ρ*g)^0.5
+        return np.sqrt(np.abs(edges[:, 5]) / (edges[:, 6] * 9.81 * self.params["D2"].value))  # To transform pressure from mΣΥ=Pa/(ρ*g) to Pa multiply denominator with ρ*g . Q=(|dP|/(K_AB*ρ*g)^0.5
     
     def sign_Q(self, edge_idx: int, ref_node_idx: int) -> float:
         '''
@@ -149,6 +173,12 @@ class createNetwork:
             sign = -1     
         
         return sign * self.edgesList[edge_idx, 7]
+    
+    def update_zeta(self, path: str):
+        '''Update the values of ζ for each edge. This function is used in the second iteration, when we know the direction of the Q of the edges.'''
+
+        new_zeta = load_data(path, "Edges")[:, 3]
+        self.edgesList[:, 8] = new_zeta
     
     def neighborNodes(self, node_idx: int) -> list:
         '''
